3722823bf8f1df1615d23ed31e55901266d269ae
sridharreddygogu <sridharreddy.gogu@gmail.com>
2023-04-07 18:24:00 +0530

    all files added

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="census_income", version=0.1,author="sridhar",author_email="sridharreddy.gogu@gmail.com", packages=find_packages(),install_requirements=["pandas","numpy","flask"])
